4ebe1c83f2d71b96c286a908891fa7587dd69440
Prashanth Palki <prashanthpalki1999@gmail.com>
2021-02-26 00:22:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { Component, useRef} from 'react';
+import { render } from 'react-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const utilizeFocus = () => {
+	const ref = React.createRef();
+	const setFocus = () => {ref.current &&  ref.current.focus()};
+	return {setFocus, ref} 
 }
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.firstRef = utilizeFocus(); 
+    this.secondRef = utilizeFocus();
+    this.thirdRef = utilizeFocus();
+    this.fourthRef = utilizeFocus();
+  }
+
+  componentDidMount(){
+    this.firstRef.setFocus();
+  }
+
+  render(){
+    return (
+      <div className="myForm mt-5">
+        <h1 style={{color:"rebeccapurple",textAlign:'center'}}>Enter OTP</h1>
+          <input type="number" maxLength="1" onChange={ (e)=>{ if (e.target.value.length===1) this.secondRef.setFocus() }} ref={this.firstRef.ref } />
+          <input type="number" maxLength="1" onChange={ (e)=>{ if (e.target.value.length===1) this.thirdRef.setFocus() }} ref={this.secondRef.ref } />
+          <input type="number" maxLength="1" onChange={ (e)=>{ if (e.target.value.length===1) this.fourthRef.setFocus() }} ref={this.thirdRef.ref } />
+          <input type="number" maxLength="1" ref={this.fourthRef.ref } />
+      </div>
+    )
+
+  } 
+}
+
+render(<App />, document.getElementById('root'));
+
 
 export default App;
